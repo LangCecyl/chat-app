9e17cbb5d7530796f35f68b239fc66278016cd7b
LangCecyl <61731319+LangCecyl@users.noreply.github.com>
2020-03-31 14:40:20 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div className="App">
+      <Contact
+        name="Tiffany Morales"
+        avatar="https://randomuser.me/api/portraits/women/76.jpg"
+      />
+    </div>
+  );
+}
+
+export default App;
